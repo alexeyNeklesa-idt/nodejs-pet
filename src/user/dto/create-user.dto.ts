006a2aa7ea832e688a7787344e04d9f7a2eab8e1
Aliaksei Niaklesa <aliaksei.niaklesa@idt.net>
2026-02-22 23:49:01 +0100

    added validation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,40 @@
+import {
+  IsArray,
+  IsEmail,
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  Length,
+} from 'class-validator';
+
+export enum UserTags {
+  ADMIN = 'admin',
+  USER = 'user',
+  GUEST = 'guest',
+}
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(2, 50)
   name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsInt()
+  @IsOptional()
+  age: number;
+
+  @IsArray()
+  @IsEnum(UserTags, { each: true })
+  @IsOptional()
+  tags: UserTags[];
 }
