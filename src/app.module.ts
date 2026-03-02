ac3edd715ddc158577dfc2c96cbedb1f1b20df06
Aliaksei Niaklesa <aliaksei.niaklesa@idt.net>
2026-03-02 21:13:04 +0100

    Added connection to posgress

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,33 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MovieModule } from './movie/movie.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, MovieModule],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: async (configService: ConfigService) => ({
+				type: 'postgres',
+				host: configService.get<string>('POSTGRES_HOST'),
+				port: configService.get<number>('POSTGRES_PORT'),
+				username: configService.get<string>('POSTGRES_USER'),
+				password: configService.get<string>('POSTGRES_PASSWORD'),
+				database: configService.get<string>('POSTGRES_DB'),
+				entities: [],
+				synchronize: true,
+				autoLoadEntities: true,
+			}),
+		}),
+		UserModule,
+		MovieModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
